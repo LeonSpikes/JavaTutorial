b474168e4c0d7f3793f695f19b85437bf1ca4bda
Leon Mavroudi <leon@mavroudi.de>
2017-08-21 14:21:24 +0200

    Added Example6

---
+++ src/Example6.java
@@ -0,0 +1,10 @@
+public class Example6 {
+    public static void main(String[] args) {
+
+        answer("Davis","Marcel",15);
+    }
+
+    public static void answer(String name, String vorname, int alter){
+        System.out.println("Die Person heißt "+vorname+" "+name+" und ist "+alter+" Jahre alt.");
+    }
+}
