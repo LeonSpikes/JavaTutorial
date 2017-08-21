003107fe59f1c7f86d30951c2686ad70acdbcda2
Leon Mavroudi <leon@mavroudi.de>
2017-08-21 13:34:48 +0200

    Added Example4.

---
+++ src/Example4.java
@@ -0,0 +1,25 @@
+public class Example4 {
+
+    //psvm
+    public static void main(String[] args) {
+
+        boolean check = false;
+
+        if(check == true){
+            correct();
+        }
+        else{
+            incorrect();
+        }
+    }
+
+    public static void correct(){
+        String textt = "correct";
+        System.out.println(textt);
+    }
+
+    public static void incorrect(){
+        String textf = "incorrect";
+        System.out.println(textf);
+    }
+}
