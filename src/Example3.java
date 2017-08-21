8e88352239822bead63b0e5bfdd1df896e0fe509
Leon Mavroudi <leon@mavroudi.de>
2017-08-21 13:18:11 +0200

    Added Example3.

---
+++ src/Example3.java
@@ -0,0 +1,19 @@
+public class Example3 {
+
+    //psvm
+    public static void main(String[] args) {
+
+        boolean check = true;
+        String textt = "it's true";
+        String textf = "it's false";
+
+        if (check == true) {
+            //sout
+            System.out.println(textt);
+        }
+        else{
+            //sout
+            System.out.println(textf);
+        }
+    }
+}
